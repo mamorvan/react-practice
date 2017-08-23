f46bf326b208df6b32b3397c1ae6e0d74d670de9
Mary Morvan <mamorvan@yahoo.com>
2017-08-23 14:18:37 -0400

    initial structure and webpack :folder:

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    //tells webpack where to get js from
+    entry: [
+        './src/index.jsx'
+    ],
+    //where webpack should output bundled file
+    output: {
+        filename: './bundle.js'
+    },
+    module: {
+        loaders: [{
+            exclude: /node_modules/,
+            loader: 'babel'
+        }]
+    }    
+};
